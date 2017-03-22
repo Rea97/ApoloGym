29289d9237143f1b72df9d585909b051373dba0a
Oziel Martinez <ozielrea97@hotmail.com>
2017-03-22 00:52:46 -0600

    Refactorizacion en vista components.topnavbar

--- resources/views/components/topnavbar.blade.php
+++ resources/views/components/topnavbar.blade.php
@@ -1,55 +1,48 @@
-<nav class="navbar navbar-default navbar-static-top">
+<nav class="nav has-shadow">
     <div class="container">
-        <div class="navbar-header">
-
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
+        <div class="nav-left">
+            <a class="nav-item" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a>
+            <!--
+            <a class="nav-item is-tab is-hidden-mobile is-active">Home</a>
+            <a class="nav-item is-tab is-hidden-mobile">Features</a>
+            <a class="nav-item is-tab is-hidden-mobile">Pricing</a>
+            <a class="nav-item is-tab is-hidden-mobile">About</a>
+            -->
         </div>
+        <span class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+        <div class="nav-right nav-menu">
+            <!--
+            <a class="nav-item is-tab is-hidden-tablet is-active">Home</a>
+            <a class="nav-item is-tab is-hidden-tablet">Features</a>
+            <a class="nav-item is-tab is-hidden-tablet">Pricing</a>
+            <a class="nav-item is-tab is-hidden-tablet">About</a>
+            -->
 
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                &nbsp;
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
+            @if (Auth::guest())
+                <a class="nav-item is-tab {{ url()->current() == route('login') ? 'is-active' : '' }}"
+                   href="{{ route('login') }}">
+                    Inicia sesión
+                </a>
+                <a class="nav-item is-tab {{ url()->current() == route('register') ? 'is-active' : '' }}"
+                   href="{{ route('register') }}">
+                    Registrate
+                </a>
+            @else
+                <a class="nav-item is-tab" href="{{ route('/') }}">{{ Auth::user()->name }}</a>
+                <a class="nav-item is-tab" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();">
+                    Logout</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            @endif
         </div>
     </div>
 </nav>
