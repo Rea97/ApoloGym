68606a4d7d1c45278c84845522e261285e9935dd
Oziel Martinez <ozielrea97@hotmail.com>
2017-03-21 21:46:34 -0600

    Agregadas traducciones de mensajes de validaciones

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe contener al menos 6 caracteres y coincidir la confirmación.',
+    'reset' => 'Tú contraseña ha sido cambiada satisfactoriamente!',
+    'sent' => 'Te hemos enviado un correo para restablecer tú contraseña!',
+    'token' => 'Este token de reseteo de contraseña es invalido.',
+    'user' => "No hemos podido encontrar un usuario con ese e-mail.",
+
+];
