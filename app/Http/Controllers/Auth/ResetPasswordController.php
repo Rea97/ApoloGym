fce300ef535bbd4da3bc6fb102d9ac076c11a0a1
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-11 16:14:18 -0500

    Corrección de errores menores
    - Soluciónado error que redireccionaba a ruta inexistente al estar logueado
    - Soluciónado error que redireccionaba a ruta inexistente al reestablecer password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard/inicio';
 
     /**
      * Create a new controller instance.
