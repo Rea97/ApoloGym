fa9e4d269243a250cf1e251307f9fa9d6e2cff33
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-11 17:21:52 -0500

    Agregada carpeta Models para las entidades de la app

---
+++ app/Models/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
