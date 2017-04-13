cc8c45ed687fb0501c62d319b039ad4b0ecde936
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-12 23:04:50 -0500

    Eliminada migración de tabla clientes

---
+++ resources/views/sections/admin/client.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oziel
+ * Date: 12/04/17
+ * Time: 03:06 AM
+ */
---
+++ resources/views/sections/admin/clients.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oziel
+ * Date: 12/04/17
+ * Time: 01:37 AM
+ */
