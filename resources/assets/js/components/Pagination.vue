24f8912df0a4ce8e2c52f5ce4b92c32bd6cf3559
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-13 20:29:34 -0500

    Refactorización en componente de paginación

--- resources/assets/js/components/Pagination.vue
+++ resources/assets/js/components/Pagination.vue
@@ -1,16 +1,18 @@
 <template>
     <ul class="pagination">
-        <li v-if="pagination.current_page > 1">
-            <a href="#" aria-label="Previous" v-on:click.prevent="changePage(pagination.current_page - 1)">
-                <span aria-hidden="true">«</span>
+        <!--<li v-if="pagination.current_page > 1">-->
+        <li :class="{'disabled':pagination.current_page == 1}">
+            <a href="#" aria-label="Previous" v-on:click.prevent="changePage('backward')">
+                <span aria-hidden="true">&laquo;</span>
             </a>
         </li>
         <li v-for="page in pagesNumber" :class="{'active': page == pagination.current_page}">
             <a href="#" v-on:click.prevent="changePage(page)">{{ page }}</a>
         </li>
-        <li v-if="pagination.current_page < pagination.last_page">
-            <a href="#" aria-label="Next" v-on:click.prevent="changePage(pagination.current_page + 1)">
-                <span aria-hidden="true">»</span>
+        <!--<li v-if="pagination.current_page < pagination.last_page">-->
+        <li :class="{'disabled':pagination.current_page == pagination.last_page}">
+            <a href="#" aria-label="Next" v-on:click.prevent="changePage('forward')">
+                <span aria-hidden="true">&raquo;</span>
             </a>
         </li>
     </ul>
@@ -49,7 +51,26 @@
         },
         methods : {
             changePage: function (page) {
-                this.pagination.current_page = page;
+                switch (page) {
+                    case 'forward':
+                        //Esta acción ocurre cuando se presipna el botón para ir hacia adelante
+                        if (this.pagination.current_page < this.pagination.last_page) {
+                            this.pagination.current_page++;
+                        }
+                        break;
+                    case 'backward':
+                        //Esta acción ocurre cuando se presipna el botón para ir hacia atrás
+                        if (this.pagination.current_page != 1) {
+                            this.pagination.current_page--;
+                        }
+                        break;
+                    default:
+                        //En caso de no recibir ninguno de los dos anteriores casos como parametros
+                        //se entiende que es un numero de página especifico recibido, por lo tanto,
+                        //se asigna directamente dicho numero a la página actual
+                        this.pagination.current_page = page;
+                        break;
+                }
             }
         }
     }
