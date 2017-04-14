537cb45728d57e00f49e17ec6faeb61ef9e3c138
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-14 03:15:25 -0500

    Refactorización en sidebar

--- resources/views/partials/dashboard/sidebar.blade.php
+++ resources/views/partials/dashboard/sidebar.blade.php
@@ -18,31 +18,31 @@
                     <i class="fa fa-home fa-fw"></i> Inicio
                 </a>
             </li>
-            <li>
-                <a href="{{ route('dashboard.routines') }}"
-                   class="{{ markAsActive(route('dashboard.routines')) }}">
-                    <i class="fa fa-th-list fa-fw"></i> Rutinas
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('dashboard.diets') }}"
-                   class="{{ markAsActive(route('dashboard.diets')) }}">
-                    <i class="fa fa-cutlery fa-fw"></i> Dietas
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('dashboard.progress') }}"
-                   class="{{ markAsActive(route('dashboard.progress')) }}">
-                    <i class="fa fa-line-chart fa-fw"></i> Progreso
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('dashboard.schedule') }}"
-                   class="{{ markAsActive(route('dashboard.schedule')) }}">
-                    <i class="fa fa-calendar fa-fw"></i> Horario
-                </a>
-            </li>
             @if(isClient())
+                <li>
+                    <a href="{{ route('dashboard.routines') }}"
+                       class="{{ markAsActive(route('dashboard.routines')) }}">
+                        <i class="fa fa-th-list fa-fw"></i> Rutinas
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('dashboard.diets') }}"
+                       class="{{ markAsActive(route('dashboard.diets')) }}">
+                        <i class="fa fa-cutlery fa-fw"></i> Dietas
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('dashboard.progress') }}"
+                       class="{{ markAsActive(route('dashboard.progress')) }}">
+                        <i class="fa fa-line-chart fa-fw"></i> Progreso
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('dashboard.schedule') }}"
+                       class="{{ markAsActive(route('dashboard.schedule')) }}">
+                        <i class="fa fa-calendar fa-fw"></i> Horario
+                    </a>
+                </li>
                 <li>
                     <a href="{{ route('dashboard.instructor') }}"
                        class="{{ markAsActive(route('dashboard.instructor')) }}">
@@ -55,6 +55,12 @@
                     <a href="{{ route('dashboard.clients') }}"
                        class="{{ markAsActive(route('dashboard.clients')) }}">
                         <i class="fa fa-users fa-fw"></i> Clientes
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('dashboard.instructors') }}"
+                       class="{{ markAsActive(route('dashboard.instructors')) }}">
+                        <i class="fa fa-users fa-fw"></i> Instructores
                     </a>
                 </li>
             @endif
