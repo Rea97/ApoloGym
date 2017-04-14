efd48657f79f8b8ff24b16efac156a5bed18ab9b
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-14 02:56:32 -0500

    Creada estructura de perfil de cliente

--- resources/views/sections/admin/client.blade.php
+++ resources/views/sections/admin/client.blade.php
@@ -3,7 +3,7 @@
 @section('title', $client->name.' '.$client->first_surname)
 @section('content')
     <div class="row">
-        <div class="col-lg-4">
+        <div class="col-sm-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <i class="fa fa-user"></i> {{ ucwords($client->name) }}
@@ -14,44 +14,79 @@
             </div>
 
         </div>
-        <div class="col-lg-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-book"></i> Información del cliente
+        <div class="col-sm-8">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-book"></i> Datos personales
+                        </div>
+                        <div class="panel-body">
+                            <h4><i class="fa fa-user-circle" aria-hidden="true"></i> Nombre(s)</h4>
+                            <p>{{ $client->name }}</p>
+                            <div class="divider"></div>
+                            <h4><i class="fa fa-users" aria-hidden="true"></i> Apellido paterno</h4>
+                            <p>{{ $client->first_surname }}</p>
+                            <div class="divider"></div>
+                            <h4><i class="fa fa-users" aria-hidden="true"></i> Apellido materno</h4>
+                            <p>{{ $client->second_surname }}</p>
+                            <div class="divider"></div>
+                            <h4>
+                                <i class="fa fa-{{ $client->gender == 'm' ? 'mars' : 'venus' }}"
+                                   aria-hidden="true"></i>
+                                Género
+                            </h4>
+                            <p>{{ $client->gender == 'm' ? 'Masculino' : 'Femenino  ' }}</p>
+                            <div class="divider"></div>
+                            <h4><i class="fa fa-calendar" aria-hidden="true"></i> Fecha de nacimiento</h4>
+                            <p>{{ $client->birth_date }}</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="panel-body">
-                    <p>Te sugerimos que llenes la siguiente información acerca de ti
-                        para poder conocerte mejor, y poder brindarte un mejor servicio.</p>
-                    <form action="">
-                        <!--  name Form Field  -->
-                        <div class="form-group">
-                            <label for="name">Nombre</label>
-                            <input id="name"
-                                   name="name"
-                                   type="text"
-                                   class="form-control input-sm"
-                                   placeholder="">
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-money"></i> Datos de facturación
                         </div>
-                        <!--  lastname Form Field  -->
-                        <div class="form-group">
-                            <label for="lastname">Apellidos</label>
-                            <input id="lastname"
-                                   name="lastname"
-                                   type="text"
-                                   class="form-control input-sm"
-                                   placeholder="">
+                        <div class="panel-body">
+                            <h4><i class="fa fa-id-card" aria-hidden="true"></i> Rfc</h4>
+                            <p>{{ $client->rfc }}</p>
+                            <div class="divider"></div>
+                            <h4><i class="fa fa-map-marker" aria-hidden="true"></i> Dirección</h4>
+                            <p>{{ $client->address }}</p>
+                            <div class="divider"></div>
+                            <h4><i class="fa fa-phone" aria-hidden="true"></i> Teléfono</h4>
+                            <p>{{ $client->phone_number }}</p>
+                            <div class="divider"></div>
+                            <h4><i class="fa fa-calendar" aria-hidden="true"></i> Fecha de último pago</h4>
+                            <p>{{ $client->birth_date }}</p>
                         </div>
-                        <!--  aboutme Form Text Area  -->
-                        <label for="aboutme">Acerca de mí</label>
-                        <textarea name="aboutme"
-                                  id="aboutme"
-                                  class="form-control"
-                                  rows="5"
-                                  placeholder="Escribe una breve descripción acerca de ti"></textarea>
-                        <span class="help-block">Texto de ayuda</span>
-
-
-                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-offset-4">
+            <div class="col-sm-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fa fa-heartbeat"></i> Datos de gimnasio
+                    </div>
+                    <div class="panel-body">
+                        <h4><i class="fa fa-male" aria-hidden="true"></i> Altura</h4>
+                        <p>{{ $client->height }} cm.</p>
+                        <div class="divider"></div>
+                        <h4><i class="fa fa-street-view" aria-hidden="true"></i> Peso</h4>
+                        <p>{{ $client->weight }} kg.</p>
+                        <div class="divider"></div>
+                        <h4><i class="fa fa-user" aria-hidden="true"></i> Instructor</h4>
+                        <p>
+                            <a href="{{url('dashboard/instructors/'.$client->instructor->id)}}">
+                                {{ $client->instructor->name }}
+                            </a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
