2329c75a6c8ffd95670730fecacbbb506515b35a
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-27 21:34:56 -0500

    Creados seeder para facturas y servicios

---
+++ database/seeds/InvoicesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InvoicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+         for ($i = 0; $i < 20; $i++) {
+            $service = \App\Models\Service::find($faker->numberBetween(1000, 1009));
+            $invoice = new \App\Models\Invoice([
+                'client_id' => $faker->numberBetween(10000, 10020),
+                'total' => $service->price,
+                'due_date' => $faker->date(),
+                'status' => $faker->randomElement(['pagada', 'sin pagar', 'parcialmente pagada', 'cancelada'])
+            ]);
+             $invoice->save();
+            $invoice->services()->attach($service->id);
+        }
+    }
+}
---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('services')->insert([
+                'name' => $faker->word,
+                'description' => $faker->paragraph(),
+                'price' => $faker->randomFloat(2, 0, 1000),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]);
+        }
+    }
+}
