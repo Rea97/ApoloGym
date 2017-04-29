ae34d5bf5bbb81ce49ff7b380ab08e55c04d56ad
Oziel Martinez <ozielrea97@hotmail.com>
2017-04-29 18:52:26 -0500

    Creado controlador de notificaciones

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Notifications\Notification;
+
+class NotificationsController extends Controller
+{
+    private $request;
+    private $response;
+
+    public function __construct(Request $request, ResponseFactory $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function index()
+    {
+        if ($this->request->ajax()) {
+            return $this->response->json([
+                'notifications' => $this->request->user()->notifications
+            ]);
+        }
+        return redirect()->route('dashboard.start');
+    }
+
+    public function unread()
+    {
+        if ($this->request->ajax()) {
+            $notifications = $this->request->get('data') == 'count' ?
+                $this->request->user()->unreadNotifications->count() :
+                $this->request->user()->unreadNotifications;
+            return $this->response->json([
+                'notifications' => $notifications
+            ]);
+        }
+        return redirect()->route('dashboard.start');
+    }
+
+    public function markAsRead($id)
+    {
+        $notification = $this->request->user()->notifications()->findOrFail($id);
+        $notification->markAsRead();
+        return $this->response->json([
+            'message' => [
+                'type' => 'success',
+                'content' => 'Notificación leída.'
+            ]
+        ]);
+    }
+}
