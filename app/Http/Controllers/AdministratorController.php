062e6121969b6f1dc832ae34c02b594341556ccb
Oziel Martinez <ozielrea97@hotmail.com>
2017-05-02 03:33:05 -0500

    Agregada funcionalidad para eliminar perfil de administrador

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Administrator;
 use Illuminate\Http\Request;
 
 class AdministratorController extends Controller
@@ -9,5 +10,23 @@ class AdministratorController extends Controller
     public function index()
     {
         return view('admin');
+    }
+
+    public function show(Request $request, Administrator $administrator)
+    {
+        if ($request->ajax()) {
+            return response()->json(['admin' => $administrator]);
+        }
+        return redirect()->route('dashboard.start');
+    }
+
+    public function delete(Administrator $administrator)
+    {
+        $administrators = Administrator::all()->count();
+        if ($administrators <= 1) {
+            return response()->json(['error' => 'Debe agregar otro administrador antes de eliminar su propio perfil.']);
+        }
+        $administrator->delete();
+        return response()->json(['success' => true]);
     }
 }
