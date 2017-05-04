3b84c2345d88fc2d46689759be47579b41736b2b
Oziel Martinez <ozielrea97@hotmail.com>
2017-05-04 00:48:00 -0500

    Agregada prop a componente AdminProfile

--- resources/views/sections/profile.blade.php
+++ resources/views/sections/profile.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Mi Perfil')
 @section('content')
     @if(isAdmin())
-        <admin-profile :admin="{{ currentAuth() }}" :show-error-alert="showErrorAlert"></admin-profile>
+        <admin-profile :admin="{{ currentAuth() }}" :show-error-alert="showErrorAlert" :alert-confirm="alertConfirm"></admin-profile>
     @elseif(isInstructor())
         Perfil de instructor
     @elseif(isClient())
