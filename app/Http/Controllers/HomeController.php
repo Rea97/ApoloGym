73f1a4239eb40a40c68576af2f6b68bde3447219
Oziel Martinez <ozielrea97@hotmail.com>
2017-05-07 18:01:03 -0500

    Refactorización en HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Repositories\FinancesRepository;
@@ -29,27 +30,35 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $year = $request->input('year', Carbon::now()->year);
-        $month = $request->input('month', Carbon::now()->month);
-        $stats = $this->finances->byDate($month, $year);
-        $incomesByCategory = $this->finances->incomesByCategory($month, $year);
-        $expensesByCategory = $this->finances->expensesByCategory($month, $year);
-        $months = [
-            1 => 'enero',2 => 'febrero',3 => 'marzo',4 => 'abril',5 => 'mayo',6 => 'junio',7 => 'julio',8 => 'agosto',9 => 'septiembre',10 => 'octubre',11 => 'noviembre',12 => 'diciembre'
-        ];
-        $summary['new_clients'] = DB::table('notifications')
-            ->where('notifiable_id', '=', $request->user()->id)
-            ->where('type', '=', 'App\Notifications\Profile\RegisteredClient')
-            ->where('read_at', '=', null)
-            ->get()
-            ->count();
-        $summary['notifications'] = $request->user()->unreadNotifications->count();
-        $summary['new_invoices'] = DB::table('notifications')
-            ->where('notifiable_id', '=', $request->user()->id)
-            ->where('type', '=', 'App\Notifications\Invoices\CreatedInvoice')
-            ->where('read_at', '=', null)
-            ->get()
-            ->count();
-        return view('sections.start', compact('stats', 'incomesByCategory', 'expensesByCategory', 'months', 'summary'));
+        if (isAdmin()) {
+            $year = $request->input('year', Carbon::now()->year);
+            $month = $request->input('month', Carbon::now()->month);
+            $stats = $this->finances->byDate($month, $year);
+            $incomesByCategory = $this->finances->incomesByCategory($month, $year);
+            $expensesByCategory = $this->finances->expensesByCategory($month, $year);
+            $months = [
+                1 => 'enero',2 => 'febrero',3 => 'marzo',4 => 'abril',5 => 'mayo',6 => 'junio',7 => 'julio',8 => 'agosto',9 => 'septiembre',10 => 'octubre',11 => 'noviembre',12 => 'diciembre'
+            ];
+            $summary['new_clients'] = DB::table('notifications')
+                ->where('notifiable_id', '=', $request->user()->id)
+                ->where('type', '=', 'App\Notifications\Clients\RegisteredClient')
+                ->where('read_at', '=', null)
+                ->get()
+                ->count();
+            $summary['notifications'] = $request->user()->unreadNotifications->count();
+            $summary['new_invoices'] = DB::table('notifications')
+                ->where('notifiable_id', '=', $request->user()->id)
+                ->where('type', '=', 'App\Notifications\Invoices\CreatedInvoice')
+                ->where('read_at', '=', null)
+                ->get()
+                ->count();
+            return view('sections.start', compact('stats', 'incomesByCategory', 'expensesByCategory', 'months', 'summary'));
+        }
+        if (isClient()) {
+            $posts = Post::all();
+            $notifications = $request->user()->unreadNotifications;
+            return view('sections.start', compact('posts', 'notifications'));
+        }
+
     }
 }
