247e7c425f2cdc212b3ad92e198e3f244989ba88
Oziel Martinez <ozielrea97@hotmail.com>
2017-05-12 17:57:37 -0500

    Corregido bug que impedía ver pdf de factura

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -169,17 +169,17 @@ class InvoicesController extends Controller
 
     public function invoiceToPdf(Invoice $invoice)
     {
-        if (! isAdmin() || currentAuth()->id != $invoice->client()->id) {
-            return redirect()->route('dashboard.start');
+        if (isAdmin() || currentAuth()->id === $invoice->client_id) {
+            $client = $invoice->client;
+            $services = $invoice->services;
+            $price['subtotal'] = $services->sum('price');
+            $price['iva'] = round($price['subtotal'] * 0.16, 2);
+            $price['total'] = $price['subtotal'] + $price['iva'];
+            $view =  \View::make('pdf.invoice', compact('invoice', 'client', 'services', 'price'))->render();
+            $pdf = \App::make('dompdf.wrapper');
+            $pdf->loadHTML($view);
+            return $pdf->stream("Factura-#{$invoice->id}-{$invoice->created_at}");
         }
-        $client = $invoice->client;
-        $services = $invoice->services;
-        $price['subtotal'] = $services->sum('price');
-        $price['iva'] = round($price['subtotal'] * 0.16, 2);
-        $price['total'] = $price['subtotal'] + $price['iva'];
-        $view =  \View::make('pdf.invoice', compact('invoice', 'client', 'services', 'price'))->render();
-        $pdf = \App::make('dompdf.wrapper');
-        $pdf->loadHTML($view);
-        return $pdf->stream("Factura-#{$invoice->id}-{$invoice->created_at}");
+        return redirect()->route('dashboard.start');
     }
 }
